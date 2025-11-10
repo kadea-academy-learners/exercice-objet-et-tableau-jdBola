2ae67a75fb17a59fa1b4e94f7836e677e03e59c2
Jean Dino <boladino56@gmail.com>
2025-11-10 21:31:28 +0100

    02 ok

--- 02-array-average.js
+++ 02-array-average.js
@@ -2,9 +2,12 @@
 // Écrire une fonction averageNote avec `notes` comme parametre qui effectue les opérations suivantes :
 // - Calcule la moyenne des notes
 // - retourne un message "Réussi" si la moyenne est >= 10, sinon "Échoué"
-
+const notes = [10, 16, 7, 9,13]
 function averageNote(notes) {
-	
+	  const total = notes.reduce((acc, note) => acc + note, 0);
+  		const moyenne = total / notes.length;
+
+  		return moyenne >= 10 ? "Réussi" : "Échoué";
 }
 
 module.exports = {
