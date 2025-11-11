ce61bf9dd4f8850d4980bdd10107ec8e2c1391a0
Jean Dino <boladino56@gmail.com>
2025-11-11 20:00:25 +0100

    03 ok

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,17 +5,19 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin(listeUtilisateurs) {
-	return utilisateurs
-    .filter(user => user.estAdmin)
-    .map(user => user.nom);
+function whoIsAdmin(utilisateurs) {
+   return utilisateurs
+    .filter(utilisateur => utilisateur.estAdmin === true)
+    .map(utilisateur => utilisateur.nom);
 }
+
 const listeUtilisateurs = [
   { nom: "Jean", age: 25, estAdmin: true },
   { nom: "Dino", age: 30, estAdmin: false },
   { nom: "Sarah", age: 22, estAdmin: true },
   { nom: "Ali", age: 28, estAdmin: false }
 ];
+
 console.log(whoIsAdmin(listeUtilisateurs));
 module.exports = {
 	whoIsAdmin,
